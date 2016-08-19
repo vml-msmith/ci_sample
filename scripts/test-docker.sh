1328b87a6dd08cca0a4a1336f326299b82255cd3
Michael Smith <Michael.Smith@vml.com>
2016-08-19 09:33:42 -0500

    Add some sleep to docker test.

--- scripts/test-docker.sh
+++ scripts/test-docker.sh
@@ -36,8 +36,9 @@ exit_code=0
 port=`docker port testdocker_app_1 80`
 port=${port#*:}
 echo ""
+sleep 5
 printf "Testing normal port 80 accesss.... "
-http_code=`curl -sL -w "%{http_code}" "localhost:${port}/phpinfo.php" -o /dev/null`
+http_code=`curl -sLk -w "%{http_code}" "localhost:${port}/phpinfo.php" -o /dev/null`
 if [ ${http_code} -eq 200 ]
 then
     printf "${GREEN}Success${NC}"
