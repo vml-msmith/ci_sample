393fbf704b43cc5777722fad542da43ef987423a
Michael Smith <Michael.Smith@vml.com>
2016-08-19 09:40:26 -0500

    More debugging on test-docekr.

--- scripts/test-docker.sh
+++ scripts/test-docker.sh
@@ -39,7 +39,7 @@ echo ""
 printf "Testing normal port 80 accesss.... "
 http_code=`curl -sLk -w "%{http_code}" "localhost:${port}/phpinfo.php" -o /dev/null`
 output=`curl -sLk "localhost:${port}/phpinfo.php"`
-echo output
+echo ${output}
 
 if [ ${http_code} -eq 200 ]
 then
